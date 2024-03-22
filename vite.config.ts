0b2000ae6492be4faf50c1895ae1d349bce64856
Alexey Filonchikov <falex@au.ru>
2024-03-22 12:34:32 +0700

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-
 // https://vitejs.dev/config/
 
 export default defineConfig({
@@ -11,7 +10,7 @@ export default defineConfig({
     outDir: 'docs'
   },
   // @ts-ignore
-  base: process.env.GH_PAGES ? '/demo-dapp-with-wallet/' : './',
+  base: process.env.GH_PAGES ? '/disintar-nft-withdraw/' : './',
   server: {
     fs: {
       allow: ['../sdk', './'],
